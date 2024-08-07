517a3e79b418e492fcb9aad0fb1760f3a3e2c13d
FRICK-ELDY <ncssano0x0000@gmail.com>
2024-08-07 21:20:09 +0900

    add avatar files

--- js/slide-world.js
+++ js/slide-world.js
@@ -1,13 +1,15 @@
+const id_name = "world"
+
 const slides = [
-  { id: "slide0", img: "./images/contents/world/stela.jpg", caption: "ステラ" },
-  { id: "slide1", img: "./images/contents/world/whale_and_waves.jpg", caption: "クジラと波" },
-  { id: "slide2", img: "./images/contents/world/you_in_a_fantastic_world.jpg", caption: "幻想的な世界に浸る、癒しの音楽" },
-  { id: "slide3", img: "./images/contents/world/4-7-8_Deep_Breath.jpg", caption: "4-7-8_Deep_Breath" },
-  { id: "slide4", img: "./images/contents/world/bird_cage_healing.jpg", caption: "鳥籠の癒し" },
-  { id: "slide5", img: "./images/contents/world/galaxy.jpg", caption: "銀河" },
-  { id: "slide6", img: "./images/contents/world/memories_on_the rooftop.jpg", caption: "屋上の思い出" },
-  { id: "slide7", img: "./images/contents/world/with_the_dusk.jpg", caption: "夕暮れと共に" },
-  { id: "slide8", img: "./images/contents/world/moonflower_butterfly.jpg", caption: "月華蝶" }
+  { id: id_name + "0", img: "./images/contents/world/stela.jpg", caption: "ステラ" },
+  { id: id_name + "1", img: "./images/contents/world/whale_and_waves.jpg", caption: "クジラと波" },
+  { id: id_name + "2", img: "./images/contents/world/you_in_a_fantastic_world.jpg", caption: "幻想的な世界に浸る、癒しの音楽" },
+  { id: id_name + "3", img: "./images/contents/world/4-7-8_Deep_Breath.jpg", caption: "4-7-8_Deep_Breath" },
+  { id: id_name + "4", img: "./images/contents/world/bird_cage_healing.jpg", caption: "鳥籠の癒し" },
+  { id: id_name + "5", img: "./images/contents/world/galaxy.jpg", caption: "銀河" },
+  { id: id_name + "6", img: "./images/contents/world/memories_on_the rooftop.jpg", caption: "屋上の思い出" },
+  { id: id_name + "7", img: "./images/contents/world/with_the_dusk.jpg", caption: "夕暮れと共に" },
+  { id: id_name + "8", img: "./images/contents/world/moonflower_butterfly.jpg", caption: "月華蝶" }
 ];
 
 function createSlide({ id, img, caption }, index, totalSlides) {
@@ -18,14 +20,14 @@ function createSlide({ id, img, caption }, index, totalSlides) {
   return `
     <div id="${id}" class="carousel-item relative w-full">
       <div>
-        <img src="${img}" class="w-full"/>
+        <img src="${img}" class="h-full"/>
       </div>
       <div class="absolute top-1 right-1 flex items-center justify-left">
         <h2 class="text-black text-2xl font-bold rounded p-8 backdrop-blur-sm bg-white/50">${caption}</h2>
       </div>
       <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide${previous}" class="btn btn-ghost">❮</a>
-        <a href="#slide${next}" class="btn btn-ghost">❯</a>
+        <a href="#${id_name}${previous}" class="btn btn-ghost">❮</a>
+        <a href="#${id_name}${next}" class="btn btn-ghost">❯</a>
       </div>
     </div>
   `;
@@ -34,7 +36,7 @@ function createSlide({ id, img, caption }, index, totalSlides) {
 function createNavigationButtons(totalSlides) {
   let buttons = '';
   for (let i = 0; i <= totalSlides; i++) {
-    buttons += `<a href="#slide${i}" class="btn btn-xs">${i}</a>`;
+    buttons += `<a href="#${id_name}${i}" class="btn btn-xs">${i}</a>`;
   }
   return buttons;
 }
